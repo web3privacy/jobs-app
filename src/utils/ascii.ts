19b09207932dce5a24944ea6fdb772aa3e167485
Adam Sobotka <vorcigernix@gmail.com>
2024-05-04 19:00:53 +0200

    fkin git

--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -2,7 +2,7 @@ import cfonts from "cfonts";
 const { render } = cfonts;
 
 export const genHeading = (str: string) => {
-    const ascii = render(str + " ", { font: "", maxLength: 5000 }, null, null, {
+    const ascii = render(str + " ", { font: "", maxLength: 5000 }, false, 0, {
         width: 5000,
         height: 5000,
     }) as { string: string };
