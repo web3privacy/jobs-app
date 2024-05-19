9bfddb077fd12a23349fb0009dc9db85ef81082b
Adam Sobotka <vorcigernix@gmail.com>
2024-05-19 18:37:25 +0200

    add page modification

--- db/config.ts
+++ db/config.ts
@@ -3,6 +3,7 @@ import { defineDb, defineTable, column } from 'astro:db';
 const Openings = defineTable({
   columns: {
     id: column.number({ primaryKey: true, autoIncrement: true }),
+    wallet: column.text(),
     jobname: column.text(),
     joburl: column.text(),
     jobcategory: column.text(),
@@ -10,10 +11,10 @@ const Openings = defineTable({
     joblocation: column.text({ optional: true }),
     companyname: column.text(),
     companyurl: column.text(),
-    companycategory: column.text(),
+    companycategory: column.text({ optional: true }),
   },
   indexes: [
-    { on: ["jobname", "companyname"], unique: false },
+    { on: ["jobname", "companyname"], unique: true },
   ]
 })
 
